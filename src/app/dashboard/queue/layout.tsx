ad396156a296d43de187c813384ad519fd0a014c
abel semahegn <abel249733@gmail.com>
2024-10-04 07:38:46 -0500

    cool background I might add to the comp pages

--- src/app/dashboard/queue/layout.tsx
+++ src/app/dashboard/queue/layout.tsx
@@ -1,9 +1,125 @@
+"use client";
 import QueueBar from "@/components/Navbar/QueueBar";
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+import { loadSlim } from "@tsparticles/slim";
+
 export default function queueLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    // Lock vertical scrolling on the entire page
+    document.body.style.overflow = "hidden";
+
+    // Unlock scrolling when component is unmounted
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, []);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "repulse",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 10,
+          },
+          repulse: {
+            distance: 200,
+            duration: 1.4,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#ffffff",
+        },
+        links: {
+          color: "#ffffff",
+          distance: 150,
+          enable: true,
+          opacity: 0.5,
+          width: 1,
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: false,
+          speed: 6,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 80,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 5 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  if (init) {
+    return (
+      <div className="w-full h-full">
+        <Particles
+          id="tsparticles"
+          particlesLoaded={particlesLoaded}
+          options={options}
+          className="w-full h-full absolute -z-10 overflow-hidden"
+        />
+        <QueueBar />
+        {children}
+      </div>
+    );
+  }
+
   return (
     <div className="w-full h-full">
       <QueueBar />
