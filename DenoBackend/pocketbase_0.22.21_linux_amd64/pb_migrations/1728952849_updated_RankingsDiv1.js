d5d27ce3fd7d42ffbd9fda2448a9917933975811
abel semahegn <abel249733@gmail.com>
2024-10-14 21:18:52 -0500

    start of routes

---
+++ DenoBackend/pocketbase_0.22.21_linux_amd64/pb_migrations/1728952849_updated_RankingsDiv1.js
@@ -0,0 +1,122 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("pv84mfgujprxste")
+
+  collection.name = "Rankings"
+
+  // remove
+  collection.schema.removeField("lbt4ve1c")
+
+  // remove
+  collection.schema.removeField("q1e9gj83")
+
+  // remove
+  collection.schema.removeField("gws0q8gq")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zdhcodb3",
+    "name": "teamName",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "steqtmml",
+    "name": "teamScore",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vndhlrce",
+    "name": "teammates",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("pv84mfgujprxste")
+
+  collection.name = "RankingsDiv1"
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "lbt4ve1c",
+    "name": "teamName",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "q1e9gj83",
+    "name": "teamScore",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "gws0q8gq",
+    "name": "teammates",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 1
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("zdhcodb3")
+
+  // remove
+  collection.schema.removeField("steqtmml")
+
+  // remove
+  collection.schema.removeField("vndhlrce")
+
+  return dao.saveCollection(collection)
+})
