21bdb720aeeb4fe2bbf5027d42413b14faac04cc
Advaiya Lad <advaiya.lad@gmail.com>
2024-11-12 23:34:31 -0600

    refactor main page, add prettier plugins, fix conflicts

---
+++ src/components/Home/_Prog.tsx
@@ -0,0 +1,17 @@
+// Shadcn has its own progress bar
+'use client';
+import { motion, useScroll, useSpring } from 'framer-motion';
+import '../../globals.css';
+export default function Progbar() {
+    const { scrollYProgress } = useScroll();
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30,
+        restDelta: 0.001,
+    });
+    return (
+        <>
+            <motion.div className="progress-bar" style={{ scaleX }} />
+        </>
+    );
+}
