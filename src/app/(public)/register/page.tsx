eb7c9ec72e55a0efb3a30bf61bdc861b493355b4
Abel Semahegn <abel249733@gmail.com>
2024-11-21 09:38:21 -0600

    good ending point untill backend is ready

--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from "react";
 import {
     Card,
@@ -10,6 +10,8 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 export default function LoginPage() {
     // State to track the current step
     const [step, setStep] = useState("choice"); // "choice", "solo", "team"
@@ -19,22 +21,14 @@ export default function LoginPage() {
             {step === "choice" && (
                 <Card className="w-fit h-fit">
                     <CardHeader>
-                        <CardTitle className="text-2xl font-bold">Sign up form</CardTitle>
-                        <CardDescription>Will you be Solo or a team?</CardDescription>
+                        <CardTitle className="text-2xl font-bold">Sign-Up Form</CardTitle>
+                        <CardDescription>Will you be Solo or a Duo?</CardDescription>
                     </CardHeader>
-                    <CardContent className="w-full justify-between flex">
-                        <Button
-                            className="bg-slate-400"
-                            onClick={() => setStep("solo")}
-                            variant={"outline"}
-                        >
+                    <CardContent className="flex gap-4 justify-between">
+                        <Button onClick={() => setStep("solo")} variant="outline">
                             Solo
                         </Button>
-                        <Button
-                            className="bg-green-400"
-                            onClick={() => setStep("team")}
-                            variant={"outline"}
-                        >
+                        <Button onClick={() => setStep("team")} variant="outline">
                             Team
                         </Button>
                     </CardContent>
@@ -49,37 +43,20 @@ export default function LoginPage() {
                     </CardHeader>
                     <CardContent>
                         <form className="flex flex-col gap-4">
-                            <Input
-                                type="text"
-                                placeholder="Name"
-                                className="pb-2 border rounded-md"
-                            />
-                            <Input
-                                type="email"
-                                placeholder="Email"
-                                className="pb-2 border rounded-md"
-                            />
-                            <Input
-                                type="password"
-                                placeholder="password"
-                                className="pb-2 border rounded-md"
-                            />
-                            <Input
-                                type="confirm"
-                                placeholder="confirm"
-                                className="pb-2 border rounded-md"
-                            />
+                            <Input type="text" placeholder="Team Name" className="pb-2 border rounded-md" />
+                            <Input type="text" placeholder="Name" className="pb-2 border rounded-md" />
+                            <Input type="email" placeholder="Email" className="pb-2 border rounded-md" />
+                            <Input type="password" placeholder="Password" className="pb-2 border rounded-md" />
+                            <Input type="password" placeholder="Confirm Password" className="pb-2 border rounded-md" />
+                            <Label htmlFor="school-id">School ID</Label>
+                            <Input id="school-id" type="file" />
                             <Button type="submit" className="bg-blue-500 text-white">
                                 Submit
                             </Button>
                         </form>
                     </CardContent>
                     <CardFooter>
-                        <Button
-                            variant={"outline"}
-                            onClick={() => setStep("choice")}
-                            className="bg-gray-300"
-                        >
+                        <Button variant="outline" onClick={() => setStep("choice")}>
                             Back
                         </Button>
                     </CardFooter>
@@ -93,67 +70,37 @@ export default function LoginPage() {
                         <CardDescription>Fill out details for each team member</CardDescription>
                     </CardHeader>
                     <CardContent>
-                        <form className="grid grid-rows-1 md:grid-flow-col gap-4">
-                            <div className="flex flex-col  gap-4">
-                                <h3 className="font-bold">Member 1</h3>
-                                <Input
-                                    type="text"
-                                    placeholder="Name"
-                                    className="pb-2 border rounded-md"
-                                />
-                                <Input
-                                    type="email"
-                                    placeholder="Email"
-                                    className="pb-2 border rounded-md"
-                                />
-                                <Input
-                                    type="password"
-                                    placeholder="password"
-                                    className="pb-2 border rounded-md"
-                                />
-                                <Input
-                                    type="confirm"
-                                    placeholder="confirm"
-                                    className="pb-2 border rounded-md"
-                                />
+                        <form className="flex flex-col gap-4">
+                            <Input type="text" placeholder="Team Name" className="pb-2 border rounded-md" />
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                <div className="flex flex-col gap-4">
+                                    <h3 className="font-bold">Member 1</h3>
+                                    <Input type="text" placeholder="Name" className="pb-2 border rounded-md" />
+                                    <Input type="email" placeholder="Email" className="pb-2 border rounded-md" />
+                                    <Input type="password" placeholder="Password" className="pb-2 border rounded-md" />
+                                    <Input type="password" placeholder="Confirm Password" className="pb-2 border rounded-md" />
+                                    <Label htmlFor="member1-id">School ID</Label>
+                                    <Input id="member1-id" type="file" />
+                                </div>
+                                <div className="flex flex-col gap-4">
+                                    <h3 className="font-bold">Member 2</h3>
+                                    <Input type="text" placeholder="Name" className="pb-2 border rounded-md" />
+                                    <Input type="email" placeholder="Email" className="pb-2 border rounded-md" />
+                                    <Input type="password" placeholder="Password" className="pb-2 border rounded-md" />
+                                    <Input type="password" placeholder="Confirm Password" className="pb-2 border rounded-md" />
+                                    <Label htmlFor="member2-id">School ID</Label>
+                                    <Input id="member2-id" type="file" />
+                                </div>
                             </div>
-                            <div className="flex flex-col  gap-4">
-                                <h3 className="font-bold">Member 2</h3>
-                                <Input
-                                    type="text"
-                                    placeholder="Name"
-                                    className="pb-2 border rounded-md"
-                                />
-                                <Input
-                                    type="email"
-                                    placeholder="Email"
-                                    className="pb-2 border rounded-md"
-                                />
-                                <Input
-                                    type="password"
-                                    placeholder="password"
-                                    className="pb-2 border rounded-md"
-                                />
-                                <Input
-                                    type="confirm"
-                                    placeholder="confirm"
-                                    className="pb-2 border rounded-md"
-                                />
-                            </div>
-               
                         </form>
                     </CardContent>
-                    <CardFooter className="w-full justify-between">
-                        <Button
-                            variant={"outline"}
-                            onClick={() => setStep("choice")}
-                            className="bg-gray-300"
-                        >
+                    <CardFooter className="flex justify-between">
+                        <Button variant="outline" onClick={() => setStep("choice")}>
                             Back
                         </Button>
-                        <Button type="submit" className="bg-blue-500 ">
-                                Submit
-                            </Button>
+                        <Button type="submit" className="bg-blue-500 text-white">
+                            Submit
+                        </Button>
                     </CardFooter>
                 </Card>
             )}
