4488a96985d7d236f93b60dd5ba47949290de6be
Abel Semahegn <abel249733@gmail.com>
2024-11-21 09:55:45 -0600

    incomming

--- src/app/(public)/(Home)/layout.tsx
+++ src/app/(public)/(Home)/layout.tsx
@@ -6,11 +6,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-<<<<<<< HEAD:src/app/(public)/layout.tsx
-        <div className=''>
-=======
         <div className="">
->>>>>>> dev:src/app/(public)/(Home)/layout.tsx
             <NavBar />
 
             {children}
