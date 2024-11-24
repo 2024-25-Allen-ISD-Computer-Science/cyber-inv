9efc2cb78cf1d293d5555906ebe19d250a16278a
Blue-Robin-Taken <julianjohnt@gmail.com>
2024-11-24 14:15:26 -0600

    added fade transition to navbar and also fixed the space-x-number in tailwind

--- src/components/nav/NavBar.tsx
+++ src/components/nav/NavBar.tsx
@@ -1,7 +1,7 @@
 import { NavLink } from '@/components/nav/NavLink';
 import Image from 'next/image';
 import Link from 'next/link';
-import * as motion from "framer-motion/client"
+import * as motion from 'framer-motion/client';
 import ico from '~/ico.svg';
 import { Button } from '../ui/button';
 import {
@@ -12,28 +12,39 @@ import {
     DrawerOverlay,
     DrawerTitle,
     DrawerTrigger,
-    DrawerPortal
-} from "@/components/ui/drawer"
+    DrawerPortal,
+} from '@/components/ui/drawer';
 
-import { GiHamburgerMenu } from "react-icons/gi";
+import { GiHamburgerMenu } from 'react-icons/gi';
 export function NavBar() {
     return (
         <>
             {/* Desktop Navbar */}
-            <nav className="hidden md:block sticky top-0 z-50 bg-black/20 backdrop-blur-md">
-                <div className="flex items-center justify-between px-8 text-center">
-                    <div className="flex items-center justify-center">
-                        <Link href="/" className="mr-4">
-                            <Image src={ico} width={64} height={64} alt="Eagle Logo" className="size-16 rounded-lg" />
-                        </Link>
-                        <Link href="/">Home</Link>
-                        <Link href="/#sponsors">Sponsors</Link>
-                        <Link href="/#faq">FAQ</Link>
-                        <Link href="/#meet-the-team">Meet the Team</Link>
-                        <Link href="/#last-year">Last Year</Link>
-                    </div>
-                    <div className="flex items-center justify-center">
-                        {/* <div className="flex flex-row gap-5 justify-center items-center text-center text-3xl text-white">
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 3 }}
+            >
+                <nav className="hidden md:block sticky top-0 z-50 bg-black/20 backdrop-blur-md">
+                    <div className="flex items-center justify-between px-8 text-center">
+                        <div className="flex items-center justify-center space-x-10 p-0.5">
+                            <Link href="/" className="mr-4">
+                                <Image
+                                    src={ico}
+                                    width={64}
+                                    height={64}
+                                    alt="Eagle Logo"
+                                    className="size-16 rounded-lg"
+                                />
+                            </Link>
+                            <Link href="/">Home</Link>
+                            <Link href="/#sponsors">Sponsors</Link>
+                            <Link href="/#faq">FAQ</Link>
+                            <Link href="/#meet-the-team">Meet the Team</Link>
+                            <Link href="/#last-year">Last Year</Link>
+                        </div>
+                        <div className="flex items-center justify-center">
+                            {/* <div className="flex flex-row gap-5 justify-center items-center text-center text-3xl text-white">
                             <motion.a
                                 href="/Signup"
                                 initial={{ opacity: 0 }}
@@ -56,52 +67,73 @@ export function NavBar() {
                                 </Button>
                             </motion.a>
                         </div> */}
+                        </div>
                     </div>
+                </nav>
+            </motion.div>
+            {/* Mobile Navbar */}
+            <nav className="block md:hidden sticky top-0 z-50 bg-black/50 backdrop-blur-md">
+                <div className="flex items-center justify-between px-8 text-center pb-4">
+                    <Drawer direction="left">
+                        <DrawerTrigger className="relative flex h-10 flex-shrink-0 items-center justify-center gap-2 overflow-hidden rounded-full px-4 text-sm font-medium shadow-sm transition-all hover:bg-[#FAFAFA] dark:bg-[#161615] dark:hover:bg-[#1A1A19] dark:text-white">
+                            <GiHamburgerMenu />
+                        </DrawerTrigger>
+                        <DrawerPortal>
+                            <DrawerContent
+                                className="h-screen left-0 w-[75vw] outline-none flex flex-col bg-black text-white"
+                                style={
+                                    {
+                                        '--initial-transform':
+                                            'translateX(-100%)',
+                                    } as React.CSSProperties
+                                }
+                            >
+                                <div className="h-full flex flex-col">
+                                    {/* Close Button */}
+                                    <div className="p-4 flex justify-end">
+                                        <DrawerClose className="text-xl text-white">
+                                            ✕
+                                        </DrawerClose>
+                                    </div>
+                                    {/* Drawer Links */}
+                                    <div className="flex flex-col gap-6 p-8">
+                                        <Link
+                                            href="/"
+                                            className="text-lg font-medium"
+                                        >
+                                            Home
+                                        </Link>
+                                        <Link
+                                            href="/#sponsors"
+                                            className="text-lg font-medium"
+                                        >
+                                            Sponsors
+                                        </Link>
+                                        <Link
+                                            href="/#faq"
+                                            className="text-lg font-medium"
+                                        >
+                                            FAQ
+                                        </Link>
+                                        <Link
+                                            href="/#meet-the-team"
+                                            className="text-lg font-medium"
+                                        >
+                                            Meet the Team
+                                        </Link>
+                                        <Link
+                                            href="/#last-year"
+                                            className="text-lg font-medium"
+                                        >
+                                            Last Year
+                                        </Link>
+                                    </div>
+                                </div>
+                            </DrawerContent>
+                        </DrawerPortal>
+                    </Drawer>
                 </div>
             </nav>
-{/* Mobile Navbar */}
-<nav className="block md:hidden sticky top-0 z-50 bg-black/50 backdrop-blur-md">
-    <div className="flex items-center justify-between px-8 text-center pb-4">
-        <Drawer direction="left">
-            <DrawerTrigger className="relative flex h-10 flex-shrink-0 items-center justify-center gap-2 overflow-hidden rounded-full px-4 text-sm font-medium shadow-sm transition-all hover:bg-[#FAFAFA] dark:bg-[#161615] dark:hover:bg-[#1A1A19] dark:text-white">
-                <GiHamburgerMenu />
-            </DrawerTrigger>
-            <DrawerPortal>
-                <DrawerContent
-                    className="h-screen left-0 w-[75vw] outline-none flex flex-col bg-black text-white"
-                    style={{ '--initial-transform': 'translateX(-100%)' } as React.CSSProperties}
-                >
-                    <div className="h-full flex flex-col">
-                        {/* Close Button */}
-                        <div className="p-4 flex justify-end">
-                            <DrawerClose className="text-xl text-white">✕</DrawerClose>
-                        </div>
-                        {/* Drawer Links */}
-                        <div className="flex flex-col gap-6 p-8">
-                            <Link href="/" className="text-lg font-medium">
-                                Home
-                            </Link>
-                            <Link href="/#sponsors" className="text-lg font-medium">
-                                Sponsors
-                            </Link>
-                            <Link href="/#faq" className="text-lg font-medium">
-                                FAQ
-                            </Link>
-                            <Link href="/#meet-the-team" className="text-lg font-medium">
-                                Meet the Team
-                            </Link>
-                            <Link href="/#last-year" className="text-lg font-medium">
-                                Last Year
-                            </Link>
-                        </div>
-                    </div>
-                </DrawerContent>
-            </DrawerPortal>
-        </Drawer>
-    </div>
-</nav>
-
-
         </>
     );
 }
