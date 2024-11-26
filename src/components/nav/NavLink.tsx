4cc9565fdbfe782ac5ea1646f468e74e78e870bc
Advaiya Lad <advaiya.lad@gmail.com>
2024-11-26 15:12:03 -0600

    Add readme

--- src/components/nav/NavLink.tsx
+++ src/components/nav/NavLink.tsx
@@ -3,24 +3,12 @@ import { cn } from '@/lib/utils';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-const smoothScrolling = (id: string) => {
-    const element = document.getElementById(id);
-    if (element) {
-        element.scrollIntoView({behavior: 'smooth'})
-    }
-}
-
 export function NavLink({ children, href }: { children: React.ReactNode; href: string }) {
     const pathname = usePathname();
     const active = pathname === href;
     return (
-        <button
-            onClick={() => smoothScrolling(href)}
-            className={cn(
-                'm-2 rounded-md p-3 text-xl text-slate-200 transition',
-            )}
-        >
+        <Link href={href} className={cn('m-2 rounded-md p-3 text-xl text-slate-200 transition')}>
             {children}
-        </button>
+        </Link>
     );
 }
