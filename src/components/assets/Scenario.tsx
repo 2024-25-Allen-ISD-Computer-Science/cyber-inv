ed67dbcf18939e342cc80aff5ac92f235dcf920e
Darrion Nguyen <darrion0920@gmail.com>
2025-01-15 10:47:18 -0600

    updated scenario round svg

--- src/components/assets/Scenario.tsx
+++ src/components/assets/Scenario.tsx
@@ -1,12 +1,16 @@
 import Image from 'next/image';
 import map from '~/map.svg' 
+import electric from '~/Electric.svg'
+import water from '~/Water.svg'
+import lines from '~/Lines.svg'
 
 export default function Scenario (){
     return(
         <div className='text-teal-50'>
-            <Image src={map} alt='map' height={700} width={900} className=''/>
+            <Image src={map} alt='map' height={700} width={900} className='relative z-0'/>
+            <Image src={electric} alt='electric' height={700} width={822.5} className='absolute z-20 mt-[-642.5px] ml-[-1px]'/>
+            <Image src={water} alt='water' height={700} width={818} className='absolute z-10 mt-[-642px] ml-[3.5px]'/>
+            <Image src={lines} alt='lines' height={700} width={810} className='absolute z-0 mt-[-487.5px] ml-[17px]'/>
         </div>
-    
- 
     )
 }
