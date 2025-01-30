de7790c80a16b577f4b4545f1851f3d1a1d83d5c
Darrion Nguyen <darrion0920@gmail.com>
2025-01-30 09:53:25 -0600

    Began working on adding command line functionality

--- src/app/(competition)/scenario/page.tsx
+++ src/app/(competition)/scenario/page.tsx
@@ -7,10 +7,28 @@ import { redirect } from 'next/navigation'// TODO: Use Suspense
 import { getRound } from "../action";
 import React, {useState} from 'react';
 import { Container } from 'lucide-react';
+import { input } from 'framer-motion/client';
+
+<style>
+  {`
+    @keyframes pulse {
+      0% {
+        transform: scale(1);
+        opacity: 0;
+      }
+      100% {
+        transform: scale(1.1);
+        opacity: 1;
+      }
+    }
+  `}
+</style>
 
 export default function page() {
     const [inputValue, setInputValue] = useState('')
     const [messages, setMessages] = useState([])
+    const [power, setPower] = useState('Normal')
+    const [water, setWater] = useState('Normal')
 
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(e.target.value);
@@ -18,16 +36,44 @@ export default function page() {
     
       const handleInputSubmit = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter' && inputValue.trim()) {
-          setMessages([...messages, inputValue]);
+          setMessages([...messages, inputValue + '\n \t ' + inputValue]);
+
+          switch (true) {
+            case inputValue.startsWith('echo'):
+                setMessages([...messages, inputValue + '\n \t ' + inputValue.substring(5, inputValue.length)])
+                break
+            default:
+                setMessages([...messages, inputValue + '\n \t' + inputValue + ' is not recognized as a command.'])
+          }
           setInputValue('');
         }
-      };
+      };    
+
+      const updateStatus = (status : String) => {
+        switch (status) {
+            case 'Normal':
+                return 'rgba(0,255,0,1) 25%, rgba(0,255,0,0.25) 50%'
+            case 'Warning':
+                return 'rgba(255,255,0,1) 25%,rgba(255,255,0,0.25) 50%'
+            case 'Critical':
+                return 'rgba(255,0,0,1) 25%, rgba(255,0,0,0.25) 50%'
+        }
+      }
+      
+      useEffect(() => {
+        const interval = setInterval(() => {
+            setPower('Critical');
+            setWater('Warning');
+        }, 1000);
+
+        return () => clearInterval(interval); // Cleanup interval on component unmount
+    }, []);
 
     useEffect(() => {
         async function checkRoundType() {
             const currentRound = await getRound();
             if (currentRound.roundType !== "scenario") {
-                redirect('/dashboard');
+                //redirect('/dashboard');
             }
         }
 
@@ -53,12 +99,12 @@ export default function page() {
                     <div className="flex flex-col h-full bg-background/60 backdrop-blur-md">
                         <div className='flex grid grid-cols-1 border-2 w-full h-[15vh] items-center justify-center'>
                             <div className='flex items-center justify-center'>
-                                <div className='w-[5vw] h-[5vh]' style={{background: "radial-gradient(circle, #66FF00 25%, rgba(0,255,0,0.25) 50%, transparent 0%)"}}/>
+                            <div className='w-[5vw] h-[5vh]' style={{background: `radial-gradient(circle, ${updateStatus(power)}, 50%, transparent 0%)`, animation: 'pulse 2s infinite'}}/>
                                 <div>Power Status</div>
                             </div>
 
                             <div className='flex items-center justify-center'>
-                                <div className='w-[5vw] h-[5vh]' style={{background: "radial-gradient(circle, #66FF00 25%, rgba(0,255,0,0.25) 50%, transparent 0%)"}}/>
+                                <div className='w-[5vw] h-[5vh]' style={{background: `radial-gradient(circle, ${updateStatus(water)}, transparent 0%)`, animation: 'pulse 2s infinite'}}/>
                                 <div>Water Status</div>
                             </div>
                         </div>
