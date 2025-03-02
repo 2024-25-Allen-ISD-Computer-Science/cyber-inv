161124fc942918e7dc4a066d285efd2b5924783d
Abel Semahegn <abel249733@gmail.com>
2025-03-02 10:04:12 -0600

    action.ts was action.tsx going to kms :skull:

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,7 @@ import type { NextRequest } from 'next/server'
 import { pb } from './lib/pocketbase'
 // This function can be marked `async` if using `await` inside
 export async function middleware(request: NextRequest) {
-  pb.authStore.onChange((token, model) => {
-    document.cookie = pb.authStore.exportToCookie({ httpOnly: true });
-});
+
   // console.log('Document Cookie:', document?.cookie)
   const response = NextResponse.next()
   const request_cookie = request.cookies.get('pb_auth')
